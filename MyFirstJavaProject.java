d1a6523af4817a17478ad44e1671bc1d63dc7ec4
kesariNavya <144530796+kesariNavya@users.noreply.github.com>
2023-09-09 16:49:43 +0530

    Add files via upload

---
+++ MyFirstJavaProject.java
@@ -0,0 +1,15 @@
+package firstproject;
+
+public class MyFirstJavaProject
+{
+    public static void main(String[] args)
+    {
+    	//variables
+    	System.out.println("integer" + Integer.SIZE);
+    	
+    	
+    	
+    	 
+    }
+}
+
